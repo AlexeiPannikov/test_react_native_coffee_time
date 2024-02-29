5083d37f20a738f2bf102d394a0e36978edf6739
alexeipannicov <alexeipannikov@gmail.com>
2024-02-29 15:22:42 +0200

    add icon and splashscreen

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
-import { SafeAreaView, StatusBar, Text } from 'react-native';
+import React, { useEffect } from 'react';
+import { Platform, SafeAreaView, StatusBar, Text } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 function App(): React.JSX.Element {
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      SplashScreen.hide();
+    }
+  }, []);
+
   return (
     <SafeAreaView>
       <StatusBar />
